7ec47b12694b92c3e060623363703df224c4d350
Pramod J <124622105+TheMtyHulk@users.noreply.github.com>
2024-06-01 12:36:22 +0530

    just fixing some stuff

---
+++ README.md
@@ -0,0 +1,3 @@
+# janvey.github.io
+Official Website for Janvey! <br>
+Jawaharlal Nehru New College of Engineering
---
+++ framerusercontent.com/sites/TOhcnwYCjAKFq0hAYTjmi/chunk-NFUTDCWG.mjs
@@ -0,0 +1,2 @@
+function t(a,e){return{description:"Cultural Fest of JNNCE",title:"JANVEY 2K24"}}export{t as a};
+//# sourceMappingURL=chunk-NFUTDCWG.mjs.map
